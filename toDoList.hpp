8c1da3a0fdd31d03633344afde55ecb6ac1f5238
Cornelius Treadway Pavlic <cornelius.pavlic@gmail.com>
2023-10-17 21:33:31 -0400

    trying again

---
+++ toDoList.hpp
@@ -0,0 +1,41 @@
+// file: toDoList.hpp
+// Author: Cornelius Pavlic
+// Purpose: A to do list for projects in school, with each being able to be sorted a variety of ways. 
+
+#ifndef TO_DO_LIST2023_HPP
+#define TO_DO_LIST2023_HPP
+#include <iostream>
+#include <string>
+
+
+
+class toDoItem
+{
+private:
+    int difficulty;
+    string dateDue;
+
+public:
+    toDoList(/* args */);
+    ~toDoList();
+};
+
+toDoList::toDoList(/* args */)
+{
+}
+
+toDoList::~toDoList()
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+#endif
